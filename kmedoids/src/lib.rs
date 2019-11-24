e5f935d0898956916934967f0fcbc9b15bb429bc
Kirk Gasper <wigasper@gmail.com>
2019-11-24 12:26:49 -0600

    .

--- kmedoids/src/lib.rs
+++ kmedoids/src/lib.rs
@@ -3,10 +3,68 @@ extern crate rand;
 use rand::Rng;
 use std::collections::HashMap;
 
-fn tester1() -> u8 {
-    4
+
+fn cost(medoids: &Vec<String>, node_labels: &HashMap<String, usize>,
+        label_indices: &HashMap<String, usize>, dist_matrix: &Vec<Vec<f64>>) -> f64 {
+    // get total cost
+    let mut total_cost = 0.0;
+    for (key, val) in node_labels.iter() {
+        // Labels are defined from medoid vector indices, so can just get the medoid
+        // for any node this way
+        let medoid = &medoids[*val];
+
+        // get indices
+        let medoid_idx = label_indices.get(medoid).unwrap();
+        let node_idx = label_indices.get(key).unwrap();
+        
+        total_cost += dist_matrix[*node_idx][*medoid_idx];
+    }
+    total_cost
+//    for medoid in medoids {
+//        let current_cluster = new_node_labels.get(medoid).unwrap();
+//        for (key, val) in new_node_labels.iter() {
+//            if val == current_cluster {
+//                let medoid_idx = label_indices.get(medoid).unwrap();
+//                let node_idx = label_indices.get(key).unwrap();
+//                total_cost += dist_matrix[*node_idx][*medoid_idx];
+//            }
+//        }
+//    }
 }
 
+fn assign_identities(medoids: &Vec<String>, node_labels: &HashMap<String, usize>,
+                     nodes: &Vec<String>, label_indices: &HashMap<String, usize>,
+                     dist_matrix: &Vec<Vec<f64>>) -> HashMap<String, usize> {
+    // Assign each medoid the label of its index in the vector
+    let mut new_node_labels = node_labels.clone();
+    for (index, medoid) in medoids.iter().enumerate() {
+        new_node_labels.insert(medoid.to_string(), index);
+    }
+
+    // For each non-medoid node, assign it the label of the closest medoid
+    for node in nodes.iter() {
+        if !medoids.contains(&node) {
+            let mut closest_medoid = medoids[0].to_string();
+            let node_idx = label_indices.get(node).unwrap();
+            let medoid_idx = label_indices.get(&closest_medoid).unwrap();
+            let mut min_dist = dist_matrix[*node_idx][*medoid_idx];
+            
+            // Check the dist of the node from each medoid
+            for medoid in medoids.iter() {
+                let node_idx = label_indices.get(node).unwrap();
+                let medoid_idx = label_indices.get(medoid).unwrap();
+                let current_dist = dist_matrix[*node_idx][*medoid_idx];
+                if current_dist < min_dist {
+                    min_dist = current_dist;
+                    closest_medoid = medoid.to_string();
+                }
+            }
+            new_node_labels.insert(node.to_string(), *new_node_labels.get(&closest_medoid).unwrap());
+        }
+    }
+
+    new_node_labels
+}
 
 #[derive(Default)]
 pub struct KMedoids {
@@ -23,7 +81,7 @@ impl KMedoids {
         Default::default()
     }
 
-    // init function to deal with input if needed
+    // Init function to deal with input if needed
     fn init(&mut self, labeled_matrix: &Vec<Vec<String>>) {
         for (index, col) in labeled_matrix[0].iter().enumerate() {
             if index != 0 {
@@ -48,44 +106,17 @@ impl KMedoids {
             }
         }
     }
-    
-    fn assign_identities(&mut self) {
-        for (index, medoid) in self.medoids.iter().enumerate() {
-            self.node_labels.insert(medoid.to_string(), index);
-        }
-        for node in self.nodes.iter() {
-            if !self.medoids.contains(&node) {
-                //let node_index = match self.label_indices.get(&node) {
-                //    Some(&index) => node_index,
-                //    _ => println!("Key error"),
-                //}
-                let mut closest_medoid = &self.medoids[0];
-                let node_idx = self.label_indices.get(node).unwrap();
-                let medoid_idx = self.label_indices.get(closest_medoid).unwrap();
-                let mut min_dist = self.dist_matrix[*node_idx][*medoid_idx];
- 
-                for medoid in self.medoids.iter() {
-                    let node_idx = self.label_indices.get(node).unwrap();
-                    let medoid_idx = self.label_indices.get(medoid).unwrap();
-                    let current_dist = self.dist_matrix[*node_idx][*medoid_idx];
-                    if current_dist < min_dist{
-                        min_dist = current_dist;
-                        closest_medoid = medoid;
-                    }
-                }
-                self.node_labels.insert(node.to_string(), *self.node_labels.get(closest_medoid).unwrap());
-            }
-        }
-    }
 
     fn fit(&mut self, k: u64) {
         let mut keys = Vec::new();
         let mut rng = rand::thread_rng();
-
+        
+        // use nodes here
         for key in self.label_indices.keys() {
             keys.push(key)
         }
-    
+        
+        // use nodes here instead of keys
         // initialize medoids, randomly choose k nodes
         //let mut init_medoids = Vec::new();
         for _ in 0..k {
@@ -93,7 +124,12 @@ impl KMedoids {
             self.medoids.push(rand_node.to_string());
         }
         
-        self.assign_identities();
+        let new_labels = assign_identities(&self.medoids, &self.node_labels, 
+                                                   &self.nodes, &self.label_indices, &self.dist_matrix);
+        
+        loop {
+            let prior_labels = new_labels.clone();
+        }
     }
 }
 
@@ -126,7 +162,79 @@ mod tests {
 
 
     }
-    fn test2() {
-        assert_eq!(tester1(), 4);
+
+    #[test]
+    fn test_assign_identities() {
+        let medoids = vec!["c".to_string(), "e".to_string()];
+        let node_labels: HashMap<String, usize>= HashMap::new();
+        let nodes = vec!["a".to_string(), "b".to_string(), "c".to_string(), "d".to_string(), "e".to_string()];
+        let mut label_indices: HashMap<String, usize> = HashMap::new();
+        label_indices.insert("a".to_string(), 0);
+        label_indices.insert("b".to_string(), 1);
+        label_indices.insert("c".to_string(), 2);
+        label_indices.insert("d".to_string(), 3);
+        label_indices.insert("e".to_string(), 4);
+        let mut dist_matrix: Vec<Vec<f64>> = Vec::new();
+        dist_matrix.push(vec![0.0, 1.0, 2.0, 3.0, 4.0]);
+        dist_matrix.push(vec![1.0, 0.0, 1.0, 2.0, 3.0]);
+        dist_matrix.push(vec![2.0, 1.0, 0.0, 4.0, 2.0]);
+        dist_matrix.push(vec![3.0, 2.0, 4.0, 0.0, 1.0]);
+        dist_matrix.push(vec![4.0, 3.0, 2.0, 1.0, 0.0]);
+        let new_labels = assign_identities(&medoids, &node_labels, &nodes, &label_indices, &dist_matrix);
+       
+        let val_0: &usize = &0;
+        let val_1: &usize = &1;
+        assert_eq!(new_labels.get(&"a".to_string()), Some(val_0));
+        assert_eq!(new_labels.get(&"b".to_string()), Some(val_0));
+        assert_eq!(new_labels.get(&"c".to_string()), Some(val_0));
+        assert_eq!(new_labels.get(&"d".to_string()), Some(val_1));
+        assert_eq!(new_labels.get(&"e".to_string()), Some(val_1));
     }
+
+    #[test]
+    fn test_cost() {
+        let medoids = vec!["c".to_string(), "e".to_string()];
+        let node_labels: HashMap<String, usize>= HashMap::new();
+        let nodes = vec!["a".to_string(), "b".to_string(), "c".to_string(), "d".to_string(), "e".to_string()];
+        let mut label_indices: HashMap<String, usize> = HashMap::new();
+        label_indices.insert("a".to_string(), 0);
+        label_indices.insert("b".to_string(), 1);
+        label_indices.insert("c".to_string(), 2);
+        label_indices.insert("d".to_string(), 3);
+        label_indices.insert("e".to_string(), 4);
+        let mut dist_matrix: Vec<Vec<f64>> = Vec::new();
+        dist_matrix.push(vec![0.0, 1.0, 2.0, 3.0, 4.0]);
+        dist_matrix.push(vec![1.0, 0.0, 1.0, 2.0, 3.0]);
+        dist_matrix.push(vec![2.0, 1.0, 0.0, 4.0, 2.0]);
+        dist_matrix.push(vec![3.0, 2.0, 4.0, 0.0, 1.0]);
+        dist_matrix.push(vec![4.0, 3.0, 2.0, 1.0, 0.0]);
+        let new_labels = assign_identities(&medoids, &node_labels, &nodes, &label_indices, &dist_matrix);
+       
+        let cost0 = cost(&medoids, &new_labels, &label_indices, &dist_matrix);
+        assert_eq!(cost0, 4.0);
+        
+        let medoids = vec!["e".to_string(), "c".to_string()];
+        let node_labels: HashMap<String, usize>= HashMap::new();
+        let nodes = vec!["a".to_string(), "b".to_string(), "c".to_string(), "d".to_string(), "e".to_string()];
+        let mut label_indices: HashMap<String, usize> = HashMap::new();
+        label_indices.insert("a".to_string(), 0);
+        label_indices.insert("b".to_string(), 1);
+        label_indices.insert("c".to_string(), 2);
+        label_indices.insert("d".to_string(), 3);
+        label_indices.insert("e".to_string(), 4);
+        let mut dist_matrix: Vec<Vec<f64>> = Vec::new();
+        dist_matrix.push(vec![0.0, 1.0, 2.0, 3.0, 4.0]);
+        dist_matrix.push(vec![1.0, 0.0, 1.0, 2.0, 3.0]);
+        dist_matrix.push(vec![2.0, 1.0, 0.0, 4.0, 2.0]);
+        dist_matrix.push(vec![3.0, 2.0, 4.0, 0.0, 1.0]);
+        dist_matrix.push(vec![4.0, 3.0, 2.0, 1.0, 0.0]);
+        let new_labels = assign_identities(&medoids, &node_labels, &nodes, &label_indices, &dist_matrix);
+       
+        let cost1 = cost(&medoids, &new_labels, &label_indices, &dist_matrix);
+        assert_eq!(cost1, 4.0);
+
+    
+    }
+
+
 }
